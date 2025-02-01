d0be0f3124421244bd0bb5efbb4d6bf850a4e387
PrarthanaKaranth <120036729+PrarthanaKaranth@users.noreply.github.com>
2025-02-01 18:24:48 +0530

    Update Error404.jsx

--- src/pages/errorPage/Error404.jsx
+++ src/pages/errorPage/Error404.jsx
@@ -8,14 +8,14 @@ const Error404 = () => {
 
     return (
         <div>
-            <Helmet><title> Error - Community Food Sharing </title></Helmet>
+            <Helmet><title> Error - ShareBite </title></Helmet>
             <div className="max-w-3xl mx-auto px-7 h-screen flex justify-center items-center">
                 <div>
                     <div>
                         <Lottie animationData={errorPage404animation}></Lottie>
                     </div>
                     <div className="mt-10 text-center">
-                        <Link className="text-primary-defaultPrimaryColor text-lg" to={'/'}> <button> Go to Hone Page </button> </Link>
+                        <Link className="text-primary-defaultPrimaryColor text-lg" to={'/'}> <button> Go to Home Page </button> </Link>
                     </div>
                 </div>
             </div>
